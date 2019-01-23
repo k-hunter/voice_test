bb29da7968b562aac86ff2e15c4526bb6a9f6af4
Mr.Masker <anonymous@github.com>
2019-01-23 11:00:32 +0800

    test of voice data send to C so and then back to app welldone ,voicedata plays well

--- main.js
+++ main.js
@@ -22,32 +22,83 @@
 	deleteButton.textContent = 'Delete';
 	deleteButton.className = 'delete';
 
-	function send_audio(audiofile)
+	var buf,str;
+	// ArrayBuffer转为字符串，参数为ArrayBuffer对象
+	function ab2str(buf)
 	{
-		return new Promise((resolve, reject) => {
-			adhoc.sendPcmVoice(audiofile);//发送话音数据
-			console.log("nnn1");
-			resolve(1);
-		});
+		//return String.fromCharCode.apply(null, new Uint16Array(buf));
+		return String.fromCharCode.apply(null, new Uint8Array(buf));
+	}
+
+	// 字符串转为ArrayBuffer对象，参数为字符串
+	function str2ab(str)
+	{
+		//var buf = new ArrayBuffer(str.length*3); // 每个字符占用2个字节
+		var buf = new ArrayBuffer(str.length); // 每个字符占用2个字节
+		//var bufView = new Uint16Array(buf);
+		var bufView = new Uint8Array(buf);
+		for (var i=0, strLen=str.length; i<strLen; i++) {
+			bufView[i] = str.charCodeAt(i);
+		}
+		return buf;
+	}
+
+	function str2arry(Str2arry)
+	{// string with ','
+		return Str2arry.split(',');//str2arry
+	}
+
+	function arry2str(Arry2str)
+	{// string with ','
+		return Arry2str.join(',');//arry2str
+	}
+
+	function blob_creator(blob_source)
+	{
+		var blob = new Blob(blob_source, { 'type' : 'audio/ogg; codecs=opus' });//get audio data{type:"text/plain"}
+		return blob;
+	}
+
+	function voicedatacallback(status, data) {
+		dump("============================================adhoc,addPcmVoiceListener==========================================");
+		dump("js callback22  navigator.mozAdhoc.addPcmVoiceListener addr="+status);
+		dump("js callback22  navigator.mozAdhoc.addPcmVoiceListener data="+data);
+		console.log("callback from C so "+data);
+		
+		//var arr_convertest = data.split(',');//str2arry
+		var arr_convertest = str2arry(data);//str2arry
+		console.log(arr_convertest);
+
+		var typedArray = new Uint8Array(arr_convertest);
+		//console.log("voicedata.length:"+typedArray.length+" data="+typedArray); 
+
+		//var string2blob = new Blob([typedArray],{ 'type' : 'audio/ogg; codecs=opus' } );//arry to blob ,and play this blob
+		var string2blob = blob_creator([typedArray]);//create blob
+		audio.controls = true;
+		var audioURL = window.URL.createObjectURL(string2blob);
+		audio.src = audioURL;
+		audio.play();
+
+	}//添加接收话音回调监听器
+
+	function arrybuffer_voice_send_recv_play(blob_exch)
+	{
+		console.log(blob_exch);
+		//将Blob 对象转换成 ArrayBuffer
+		var reader = new FileReader();
+		//reader.readAsArrayBuffer(blob_exch,'utf-16');
+		reader.readAsArrayBuffer(blob_exch,'utf-8');
+		reader.onload = function (e) {
+			var abf=reader.result;
+			var typedArray = new Uint8Array(abf);
+			console.log(typedArray); 
+			var str_vt = arry2str(typedArray);//arrybuffer to string 
+			console.log(str_vt);
+			adhoc.addPcmVoiceListener_spe_api(voicedatacallback,str_vt);
+		}
 	}
 
 
-	function get_audio()
-	{
-		console.log("nnn3");
-		adhoc.addPcmVoiceListener(function(status, data) {
-			//setTimeout(
-			dump("============================================adhoc,addPcmVoiceListener==========================================");
-			dump("js callback22  navigator.mozAdhoc.addPcmVoiceListener addr="+status);
-			dump("js callback22  navigator.mozAdhoc.addPcmVoiceListener data="+data);
-			//2000);
-		});//添加接收话音回调监听器
-
-	}
-
-
-
-	var audio_file_play;
 	function recorder(){
 		var onSuccess = function(stream) {
 
@@ -56,7 +107,6 @@
 				//console.log(mediaRecorder.state);//recording
 				console.log("开始录音");
 				record.style.background = "red";
-
 				stop.disabled = false;
 				record.disabled = true;
 			}
@@ -66,98 +116,20 @@
 				console.log("停止录音");
 				record.style.background = "";
 				record.style.color = "";
-
 				stop.disabled = true;
 				record.disabled = false;
 			}
 			var mediaRecorder = new MediaRecorder(stream);
 			mediaRecorder.onstop = function(e) {//when stop happen, do this 
-				//console.log("data available after MediaRecorder.stop() called.");
 				var audio = document.createElement('audio');
 				audio.controls = true;
 
-				//var sdcard = navigator.getDeviceStorage("sdcard");
-				var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });//get audio data
-				//var file   = new Blob(["This is a text file."], {type: "text/plain"});
-				//console.log(blob);
-				//console.log(file);
-
+				var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });//get audio data{type:"text/plain"}
+				var abf=blob;
+				//实际调用,测试转换情况及是否可用，
+				arrybuffer_voice_send_recv_play(blob);
+				//arrybuffer_voice_play(blob);//well done
 				chunks = [];
-				var reader=new FileReader();
-				reader.readAsText(blob,'utf-8');
-				reader.onload=function(e)
-				{
-					var textfile=reader.result;
-					//console.log(textfile);
-					//console.log(reader);
-					
-					send_audio(textfile).then(() => {
-						console.log("nnn2");
-						get_audio();
-					});
-				}
-
-				//将Blob 对象转换成 ArrayBuffer
-/*
- *                reader.readAsArrayBuffer(blob);
- *                reader.onload = function (e) { //将 ArrayBufferView  转换成Blob
- *                    var buf = new Uint8Array(reader.result);
- *                    console.info(buf); //[228, 184, 173, 230, 150, 135, 229, 173, 151, 231, 172, 166, 228, 184, 178]
- *
- *                    reader.readAsText(new Blob([buf]), 'utf-8');
- *                    reader.onload = function () {
- *
- *                        var string2blob = new Blob([buf], {
- *                            type: 'text/plain'
- *                        });
- *
- *                        console.info(string2blob); //中文字符串
- *                        adhoc.sendPcmVoice(string2blob);//发送话音数据
- *                    };
- *
- *                    var buf = new DataView(reader.result); //将 ArrayBufferView  转换成Blob
- *                    console.info(buf); //DataView {}
- *                    reader.readAsText(new Blob([buf]), 'utf-8');
- *                    reader.onload = function () {
- *                        console.info(reader.result); //中文字符串
- *                    };
- *                }
- */
-
-
-
-				/*
-				 *var request = sdcard.addNamed(blob, "audio_file1.ogg");
-				 *request.onsuccess = function () {
-				 *    var name = this.result;
-				 *    console.log('File "' + name + '" successfully wrote on the sdcard storage area');
-				 *}
-				 */
-
-				// An error typically occur if a file with the same name already exist
-				/*
-				 *request.onerror = function () {
-				 *    console.warn('Unable to write the file: ' + this.error);
-				 *}
-				 */
-
-				/*
-				 *                var request_get = sdcard.get("audio_file1.ogg");
-				 *
-				 *                request_get.onsuccess = function () {
-				 *                    audio_file_play = this.result;
-				 *                    console.log("Get the file: " + audio_file_play.name);
-				 *                    console.log(audio_file_play);
-				 *                    var audioURL = window.URL.createObjectURL(audio_file_play);
-				 *                    console.log(audioURL);
-				 *                    audio.src = audioURL;
-				 *                    audio.play();
-				 *                }
-				 *                request_get.onerror = function () {
-				 *                    console.warn("Unable to get the file: " + this.error);
-				 *                }
-				 *
-				 */
 
 				clipContainer.appendChild(audio);
 				clipContainer.appendChild(clipLabel);
@@ -168,20 +140,13 @@
 					evtTgt = e.target;
 					evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
 				}
-
 				audio.controls = true;
-				//console.log("recorder stopped");
 			}
 
 			mediaRecorder.ondataavailable = function(e) {
 				chunks.push(e.data);
-				var textfile1=chunks;
-				//console.log("\ntextfile1:\n"+textfile1);
-				//console.log("reader\n"+reader.result);
-				console.log(chunks);
 			}
 		}//onsuceed
-
 
 		var onError = function(err) {
 			console.log("错误出现在：　"　+ err);
@@ -189,117 +154,78 @@
 		navigator.mediaDevices.getUserMedia(constraints).then(onSuccess,onError);
 	}//recorder
 
-	function save_audio_file()
-	{
-
-		var sdcard = navigator.getDeviceStorage("sdcard");
-		var file   = new Blob(["This is a text file."], {type: "text/plain"});
-
-		//console.log(sdcard);
-		var request = sdcard.addNamed(file, "my-file.txt");
-
-		request.onsuccess = function () {
-			var name = this.result;
-			console.log('File "' + name + '" successfully wrote on the sdcard storage area');
-		}
-
-		// An error typically occur if a file with the same name already exist
-		request.onerror = function () {
-			console.warn('Unable to write the file: ' + this.error);
-		}
-
-	}
-
-	function send_audio_file()
-	{
-		//将字符串转换成 Blob对象
-		var blob = new Blob(['中文字符串'], {
-			type: 'text/plain'
-		});
-		console.log(blob);
+	function arrybuffer_voice_play(blob_exch)
+	{//test,可正常播放,不经过so库，原地转换测试
+		console.log(blob_exch);
+		var ab_exch,str_exch;
 		//将Blob 对象转换成 ArrayBuffer
 		var reader = new FileReader();
-		reader.readAsArrayBuffer(blob);
+		//reader.readAsArrayBuffer(blob_exch,'utf-16');
+		reader.readAsArrayBuffer(blob_exch,'utf-8');
 		reader.onload = function (e) {
-			//console.info(reader.result); //ArrayBuffer {}
+			var abf=reader.result;
+			console.log(abf); 
+			var typedArray = new Uint8Array(abf);
+			console.log(typedArray); 
+			console.log("voicedata.length:"+typedArray.length); // 3
+			var string1=ab2str(abf);
+			ab_exch=str2ab(string1);
+			console.log(ab_exch);
 
-
-			//将 ArrayBufferView  转换成Blob
-			var buf = new Uint8Array(reader.result);
-			console.info(buf); //[228, 184, 173, 230, 150, 135, 229, 173, 151, 231, 172, 166, 228, 184, 178]
-
-			reader.readAsText(new Blob([buf]), 'utf-8');
-			reader.onload = function () {
-
-				var string2blob = new Blob([buf], {
-					type: 'text/plain'
-				});
-
-				//console.info(reader.result); //中文字符串
-				console.info(string2blob); //中文字符串
-				adhoc.sendPcmVoice(string2blob);//发送话音数据
-			};
-
-			//将 ArrayBufferView  转换成Blob
-			/*
-			 *var buf = new DataView(reader.result);
-			 *console.info(buf); //DataView {}
-			 *reader.readAsText(new Blob([buf]), 'utf-8');
-			 *reader.onload = function () {
-			 *    console.info(reader.result); //中文字符串
-			 *};
-			 */
-		}
-
-	}
-
-	function play_audio_file()
-	{
-		clipContainer.appendChild(audio);
-		clipContainer.appendChild(clipLabel);
-		clipContainer.appendChild(deleteButton);
-		soundClips.appendChild(clipContainer);
-		//var audio_file_play;
-
-		var sdcard = navigator.getDeviceStorage('sdcard');
-		//console.log(sdcard);
-		var request = sdcard.get("audio_file1.ogg");
-
-		audio.controls = true;
-		request.onsuccess = function () {
-			audio_file_play = this.result;
-			//console.log("Get the file: " + audio_file_play.name);
-			var audioURL = window.URL.createObjectURL(audio_file_play);
+			//var string2blob = new Blob([ab_exch],{ 'type' : 'audio/ogg; codecs=opus' } );
+			var string2blob = new Blob([typedArray],{ 'type' : 'audio/ogg; codecs=opus' } );
+			audio.controls = true;
+			var audioURL = window.URL.createObjectURL(string2blob);
 			audio.src = audioURL;
 			audio.play();
 		}
-		deleteButton.onclick = function(e) {
-			evtTgt = e.target;
-			evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-		}
-
-
-		request.onerror = function () {
-			console.warn("Unable to get the file: " + this.error);
-		}
-
-
-
-		adhoc.addPcmVoiceListener(function(status, data) {
-			dump("============================================adhoc,addPcmVoiceListener==========================================");
-			dump("js callback22  navigator.mozAdhoc.addPcmVoiceListener addr="+status);
-			dump("js callback22  navigator.mozAdhoc.addPcmVoiceListener data="+data);
-		});//添加接收话音回调监听器
-
-
 	}
+	
+	
+	function adhoc_tellip()
+	{
+		var convertest = "hellohhahahahahahahahhsffffffffffanananananananiiiiiiiiiiiiiii";
+		//var convertest = "hellohhahahahahahahahh from jsffffffffffanananananananiiiiiiiiiiiiiii";
+		//adhoc.tellLocalIpAddr(convertest);//设置本机自组网ip
+		//adhoc.sendPcmVoice(convertest);//发送话音数据
+		console.log(convertest);
+		adhoc.addPcmVoiceListener_spe_api(voicedatacallback,convertest);
+	}
+
+	function fkab_voice(){
+
+		var convertest = "11,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66,44,77,008,66";
+		var arr_convertest = convertest.split(',');//str2arry
+		console.log(arr_convertest);
+		adhoc.addPcmVoiceListener_spe_api(voicedatacallback,convertest);
+		//var str_vt = arr_convertest.join(',');
+		//console.log(str_vt);
+	}
+
+	function fkab_test(){
+
+		var typedArray = new Uint8Array([0,1,2]);
+		console.log(typedArray); 
+		console.log(typedArray.length); // 3
+		typedArray[0] = 5;
+		console.log(typedArray); // [5, 1, 2]
+	}
+
+
+
+
 
 	function test()
-	{
-		var peerConnection = new mozRTCPeerConnection();
-		console.log(peerConnection);
-		//recorder();
+	{	
+		//var test_string="hia hie hia";
+		//dump("i                                                   adhoc js :"+test_string);
+		//adhoc.addPcmVoiceListener_spe_api(voicedatacallback,test_string);
+		//adhoc_tellip();
+		//fkab_test();
+		fkab_voice();
 	}
+
+
 
 	$(document).ready(function () {
 
@@ -324,25 +250,20 @@
 
 		$("#Send").on('click',function(e){
 
-			send_audio_file();
 			console.log("send");
 		});
-
 
 
 		$("#Play").on('click',function(e){
 
 			console.log("play");
-			play_audio_file();
 		});
 
 		$("#Test").on('click',function(e){
 			//console.log("test");
 			//recorder();
-			//test();
+			test();
 		});
-
-
 
 	});
 
